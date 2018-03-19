41c85eeca61ac6f6c5f6ad5c875cfc916408ee8e
Jesus Moris <jesus@soluciontotal.cl>
2018-03-19 19:15:50 -0300

    Se agrega CSS mejorado.

--- src/DTE.php
+++ src/DTE.php
@@ -23,87 +23,54 @@ class DTE {
 
     private function setCss(){
         $css = '
-                .factura {
-                    font-size: 8pt;
-                }
+        .factura {
+            width: 100%;
+            margin: 2cm;
+        }
+        
+        .info-emisor {
+            width: 100%;
+        }
+        
+        .info-emisor .logo{
+            width: 30%;
+            vertical-align: 60;
+            display: inline-block;
+        }
+        
+        .info-emisor .logo img {
+            width: 100%;
+            vertical-align: middle;
+        }
 
-                .razonsocial {
-                    margin: 0px;
-                    padding: 0px;
-                    font-weight: bold;
-                }
-                
-                .masinfo {
-                      margin: 0px;
-                      padding: 0px;
-                      font-size: 10px;
-                }
-
-                .bordes {
-                    border-left: 1px solid black;
-                    border-right: 1px solid black;
-                    border-top: 1px solid black;
-                    border-bottom: 1px solid black;
-                }
-
-                .tabla-header {
-                    width: 100%;
-                }
-
-                .detalles {
-                    text-align: center;
-                    width: 100%;
-                }
-
-                .detalles tr th {
-                    border-left: 1px solid black;
-                    border-right: 1px solid black;
-                    border-top: 1px solid black;
-                    border-bottom: 1px solid black;
-                    font-weight: bold;
-                } 
-
-                .detalles tr td {
-                    border-right: 1px solid black;
-                }
-
-                .info-emisor {
-                    width: 100%;
-                    height: 100px;
-                }
-
-                .info-emisor .logo {
-                    display: inline-block;
-                    width: 28%;
-                }
-
-                .info-emisor .logo img {
-                    position:absolute;
-                    top:50%;
-                    left:50%;
-                    width: 100%;
-                }
-
-                .info-emisor .info {
-                    display: inline-block;
-                    width: 40%;
-                }
-
-                .info-emisor .cuadro {
-                    display: inline-block;
-                    width: 30%;
-                }
-
-                .info-emisor .cuadro .cuadro-rojo {
-                    border-left: 1px solid red;
-                    border-right: 1px solid red;
-                    border-top: 1px solid red;
-                    border-bottom: 1px solid red;
-                }
-
-                .espacio {
-                    padding-bottom: 5px;
-                }
+        .info-emisor .info{
+            width: 30%;
+            vertical-align: top;
+            display: inline-block;
+        }
+        
+        .info-emisor .info .razonsocial {
+            font-size: 10px;
+            font-weight: bold;
+            margin:0;
+            padding:0;
+        }
+        
+        .info-emisor .info .masinfo {
+            font-size: 8px;
+            margin:0;
+            padding:0;
+        }
+        
+        .info-emisor .cuadro{
+            width: 30%;
+            display: inline-block;
+        }
+        .info-emisor .cuadro .cuadro-rojo {
+            font-size: 10px;
+            border: 3px solid red;
+            text-align: center;
+        }
         ';
         return $css;
     }

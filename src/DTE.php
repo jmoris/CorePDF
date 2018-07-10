c7ab65ccc3c4ea5c31fdd1c7e84f8f4b2e9e0980
Jesus Moris <jesus@soluciontotal.cl>
2018-07-10 01:25:29 -0400

    fix wordwrap

--- src/DTE.php
+++ src/DTE.php
@@ -402,10 +402,6 @@ class DTE {
                     line-height: 0.1;
                     margin-left: 5px;
                 }
-                .emisor .giro {
-                    font-family: Arial, "Helvetica Neue", Helvetica, sans-serif; font-size: 80%;
-                    word-wrap: break-word;
-                }
                 .receptor {
                     font-family: Arial, "Helvetica Neue", Helvetica, sans-serif; font-size: 80%; 
                     line-height: 0.1;
@@ -832,7 +828,7 @@ class DTE {
         $html .= '</div>';
         $html .= '<div class="emisor">';
         $html .= '<p><b>'.$this->dte['Encabezado']['Emisor']['RznSoc'].'</b></p>';
-        $html .= '<p class="giro">'.$this->dte['Encabezado']['Emisor']['GiroEmis'].'</p>';
+        $html .= '<p style="word-wrap: break-word;">'.$this->dte['Encabezado']['Emisor']['GiroEmis'].'</p>';
         $html .= '<p>'.$this->dte['Encabezado']['Emisor']['DirOrigen'].', '.$this->dte['Encabezado']['Emisor']['CmnaOrigen'].'</p>';
         $html .= $this->telefono;
         $html .= $this->mail;

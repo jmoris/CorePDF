28e647e0fb14a80522ffc1b904e678c01ed338d0
Jesus Moris <jesus@soluciontotal.cl>
2018-07-15 00:56:48 -0400

    se quita relleno superior e inferior

--- src/DTE.php
+++ src/DTE.php
@@ -833,7 +833,7 @@ class DTE {
         $html .= '</div>';
         $html .= '<div class="emisor">';
         $html .= '<p><b>'.$this->dte['Encabezado']['Emisor'][($tipo == 39) ? 'RznSocEmisor':'RznSoc'].'</b></p>';
-        $html .= '<p style="word-wrap: break-word; line-height: normal;">'.$this->dte['Encabezado']['Emisor'][($tipo == 39) ? 'GiroEmisor' : 'GiroEmis'].'</p>';
+        $html .= '<p style="padding-top: 0; padding-bottom: 0;word-wrap: break-word; line-height: normal;">'.$this->dte['Encabezado']['Emisor'][($tipo == 39) ? 'GiroEmisor' : 'GiroEmis'].'</p>';
         $html .= '<p>'.$this->dte['Encabezado']['Emisor']['DirOrigen'].', '.$this->dte['Encabezado']['Emisor']['CmnaOrigen'].'</p>';
         $html .= $this->telefono;
         $html .= $this->mail;
